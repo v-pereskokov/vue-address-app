26991177fd8174634da57ff884b4334c7dbbbeb1
vladpereskokov <vladpereskokov@mail.ru>
2017-07-15 17:07:13 +0300

    add style input

--- src/app/components/token-input/token-input.component.ts
+++ src/app/components/token-input/token-input.component.ts
@@ -1,14 +1,26 @@
 import * as Vue from 'vue';
 import Component from 'vue-class-component';
+import { addresses } from '../../lib/addresses';
 
+import './token-input.component.scss';
 
 @Component({
-  template: `<input @keyup="eventSource" v-model="token" />`,
+  template: `<div class="row">
+              <form class="col s12">
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input placeholder="Search" class="token-input" @keyup="eventSource" v-model="token" />
+                  </div>
+                </div>
+              </form>
+            </div>`,
 })
 export class TokenInputComponent extends Vue {
   public token: string = '';
 
-  public eventSource() {
+  public async eventSource() {
+    const data = await addresses.getAddressByToken('abc');
+    console.log(data);
   }
 
 }
